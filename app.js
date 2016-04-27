3ae87b0874038211b6d1a3be06e827f9e9bf37ed
Clicia Scarlet <yuyuvn@mac.com>
2016-04-27 21:21:43 +0700

    Init

---
+++ app.js
@@ -0,0 +1,62 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var server = restify.createServer();
+var google = require('googleapis')
+var OAuth2 = google.auth.OAuth2
+var oauth2Client = null
+var bot = new builder.BotConnectorBot();
+bot.add('/', new builder.CommandDialog()
+  .matches('^gen google oauth2?', builder.DialogAction.beginDialog('/google-oauth2'))
+  .matches('^quit', builder.DialogAction.endDialog())
+  .onDefault(function (session) {
+    session.beginDialog('/google-oauth2');
+  }));
+bot.add('/google-oauth2',  [
+  function (session) {
+    builder.Prompts.text(session, "What is your CLIENT_ID?\n\
+      You can get it from http://console.developers.google.com/\n\
+      You should add "+process.env.GOOGLE_OAUTH2_REDIRECT+" to your host list");
+  },
+  function (session, results) {
+    session.userData.client_id = results.response
+    builder.Prompts.text(session, "What is your CLIENT_SECRET?");
+  },
+  function (session, results) {
+    session.userData.client_secret = results.response
+    builder.Prompts.text(session, "What scopes do you want?");
+    oauth2Client = new OAuth2(session.userData.client_id, session.userData.client_secret, process.env.GOOGLE_OAUTH2_REDIRECT);
+  },
+  function (session, results) {
+    session.userData.scope = results.response.split(",")
+    url = oauth2Client.generateAuthUrl({access_type: 'offline', scope: results.response})
+    builder.Prompts.text(session, "Please go to "+url+" then parse your code here");
+  },
+  function (session, results) {
+    oauth2Client.getToken(results.response, function(err, tokens){
+      if (err) {
+        session.send('Error: %s', err);
+      } else {
+        text = ""
+        for (key in tokens) {
+          text += key + ": " + tokens[key] + "\n";
+        }
+        session.send(text);
+      }
+    });
+  }
+]);
+
+server.use(restify.queryParser());
+server.use(bot.verifyBotFramework({appId: process.env.BOTFRAMEWORK_APPID, appSecret: process.env.BOTFRAMEWORK_APPSECRET }));
+server.post('/v1/messages', bot.listen());
+server.get('/google/oauth2', function(req, res){
+  res.send(req.query.code);
+});
+server.get('/keepalive', function(req, res){
+  res.send("OK");
+});
+
+server.listen(process.env.port || 3978, function () {
+  console.log('%s listening to %s', server.name, server.url);
+});
